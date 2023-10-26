510b1d4bad9b2ffc22de0608ddd4b6808c4a661d
Gowtham212000 <gowthmrishi@gmail.com>
2023-10-26 21:18:54 +0530

    first commit

---
+++ resume.js
@@ -0,0 +1,35 @@
+let myResume={
+    "basics": {
+      "name": "gowtham N",
+      "email": "gowthmrishi@gmail.com",
+      "degree": "DIT",
+      "location": {
+        "address": "karuppanasamy kovil street",
+        "postalCode": "624705",
+        "city": "kannivadi",
+        "district":"Dindigul",
+        "state": "TamilnadU",
+        "country": "India"
+      },
+    },
+    "education": [
+      {
+        "institution": "Arulmigu palaniyadavar Polytechnic College",
+        "department": "INFORMATION TECHNOLOGY",    
+        "batch start year": 2017,
+        "batch end year": 2020,
+      }
+    ],
+    "skills": [
+      {
+        "name": "web developer",
+            "level": "beginer",}
+    
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+  }
+  console.log(myResume);
---
+++ task5.js
@@ -0,0 +1,32 @@
+var json = [{
+    "name" : "Gowtham", 
+    "msg"   : "For the given JSON iterate over all for loops (for, for in, for of, forEach)",
+    "mail": "gowthmrishi@gamil.com"
+
+}];
+//for loop
+for(var i = 0; i < json.length; i++) {
+    var obj = json[i];
+
+    console.log(obj.name);
+    console.log(obj.msg);
+    console.log(obj.mail);
+
+}
+//for Each
+json.forEach(function(obj) { console.log(obj.msg); });
+
+//for In
+for (var key in json) {
+if (json.hasOwnProperty(key)) {
+  console.log(json[key].name);
+  //console.log(json[key].msg);
+
+}
+}
+//for Of
+let text = "";
+for (let x of json[key].name) {
+ text += x; 
+}
+ console.log(text);
